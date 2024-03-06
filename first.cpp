d1316f931db87c5156dd03189baf776d92c44596
NandiniPavale <124230256+NandiniPavale@users.noreply.github.com>
2024-03-06 21:26:33 +0530

    project

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+int sum;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+sum = x + y;
+cout << "Sum is: " << sum;
+
+}
